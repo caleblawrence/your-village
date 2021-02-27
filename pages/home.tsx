e65abf9dfbdafc071435dc41789589c0b3090dde
Caleb Lawrence <lawrence.calebc@gmail.com>
2021-02-27 17:38:37 -0600

    added datetime picker to home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,19 +1,46 @@
 import useUser from "../lib/useUser";
 import Layout from "../components/Layout";
 import { IUser } from "../types/IUser";
+import { Paper } from "@material-ui/core";
+import { useState } from "react";
+import * as React from "react";
+import TextField from "@material-ui/core/TextField";
+import { DateTimePicker } from "@material-ui/pickers";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
 const Home = () => {
   let data = useUser({ redirectTo: "/login" });
   let user: IUser = data.user;
+  const [selectedDate, handleDateChange] = useState<Date | null>(new Date());
 
   if (!user || user.isLoggedIn === false) {
     return <Layout>loading...</Layout>;
   }
 
   return (
-    <Layout>
-      <h1>Home</h1>
-    </Layout>
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Layout>
+        <h1>Home</h1>
+        <Paper
+          elevation={3}
+          style={{
+            padding: 15,
+            backgroundColor: "rgb(28 29 33)",
+            marginTop: 20,
+          }}
+        >
+          <h1>Enter a date you need free</h1>
+          <DateTimePicker
+            // @ts-expect-error
+            renderInput={(props) => <TextField variant="outlined" {...props} />}
+            label="DateTimePicker"
+            value={selectedDate}
+            onChange={handleDateChange}
+          />
+        </Paper>
+      </Layout>
+    </MuiPickersUtilsProvider>
   );
 };
 
