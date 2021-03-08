2986659206deec014e8b7c075a6caa6ef086697a
Caleb Lawrence <lawrence.calebc@gmail.com>
2021-03-07 22:42:17 -0600

    test bug fix

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,7 +1,9 @@
 import withSession from "../../lib/session";
 
 export default withSession(async (req, res, session) => {
-  req.session.set("user", null);
+  req.session = null;
+  await req.session.save();
+
   req.session.destroy();
   return res.status(200).json({ message: "Logged out" });
 });
